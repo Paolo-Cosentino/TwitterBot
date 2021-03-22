dc80d0c52fa5f7d4a45194f360695d7945558eb8
Paolo Cosentino <pcosentino95@gmail.com>
2021-03-22 01:06:02 -0700

    refactor

--- bot.py
+++ bot.py
@@ -19,7 +19,6 @@ CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
 ACCESS_KEY = os.environ['ACCESS_KEY']
 ACCESS_SECRET = os.environ['ACCESS_SECRET']
 
-
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
