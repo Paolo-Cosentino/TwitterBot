53a53e8974ffcc49fd660920e641e7fcd84324e3
Paolo Cosentino <pcosentino95@gmail.com>
2021-05-11 03:56:29 -0700

    refactor

--- server.py
+++ server.py
@@ -12,7 +12,6 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 
 
-
 class TweetSchema(ma.Schema):
     class Meta:
         fields = ('id',
@@ -33,43 +32,21 @@ def home():
     all_tweets = session.query(Tweet).all()
     # print(all_tweets)
     session.close()
-    return render_template("index.html", tweets=str(all_tweets), count=len(all_tweets))
+    return render_template("index.html", tweets=all_tweets, count=len(all_tweets))
 
 
-# @app.route('/', methods=['POST'])
-# def screen_name_lookup():
-#     screen_name = request.form['screen_name'].replace(" ", "_")
-#     if request.form.get('display-json'):
-#         return redirect(url_for('get_tweets_by_screen_name', layout='json', screen_name=screen_name))
-#     return redirect(url_for('get_tweets_by_screen_name', layout='table', screen_name=screen_name))
+@app.route('/', methods=['POST', 'GET'])
+def screen_name_lookup():
+    screen_name = request.form['screen_name'].replace(" ", "_")
+    print(screen_name)
+    session = DBSession()
+    user_tweets = session.query(Tweet).filter(
+        func.lower(Tweet.screen_name) == screen_name.lower()).all()
+    user_tweets_length = len(user_tweets)
+    session.close()
+    if user_tweets_length == 0:
+        return jsonify(results=f'No data found for: {screen_name}')
+    return tweets_schema.jsonify(user_tweets)
 
-
-# @app.route('/<layout>/<screen_name>/', methods=['GET'])
-# def get_tweets_by_screen_name(layout, screen_name):
-#     if screen_name == '*':  # All tweets
-#         if layout == 'json':
-#             all_tweets = session.query(Tweet)
-#             return tweets_schema.jsonify(all_tweets)
-#         else:  # Table view for all tweets
-#             all_tweets = session.query(Tweet)
-#             return render_template('success.html', json_data=all_tweets, sn='*')
-#     else:  # Tweets searched by screen_name
-#         if layout == 'json':
-#             user_tweets = session.query(Tweet).filter(func.lower(Tweet.screen_name) == screen_name.lower()).all()
-#             user_tweets_length = len(user_tweets)
-#             if user_tweets_length == 0:
-#                 return jsonify(results=f'No data found for: {screen_name}')
-#             return tweets_schema.jsonify(user_tweets)
-#         else:  # Table view for search by screen_name
-#             user_tweets = session.query(Tweet).filter(func.lower(Tweet.screen_name) == screen_name.lower()).all()
-#             user_tweets_length = len(user_tweets)
-
-#             if user_tweets_length == 0:
-#                 return render_template('oops.html')
-#             return render_template(
-#                 'success.html',
-#                 json_data=user_tweets,
-#                 sn=screen_name
-            # )
 
 app.run(host='0.0.0.0', port=os.environ.get('PORT'))
